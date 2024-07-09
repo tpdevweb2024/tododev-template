26a37c10c7cbf979bd7cf8434a3332231264709d
anthocodeur <antho.codeur@gmail.com>
2024-07-09 14:25:57 +0200

    event listner eye pass

--- register.php
+++ register.php
@@ -18,7 +18,16 @@
                 <div class="error_email"></div>
             </div>
             <div class="input_pass">
-                <input type="password" name="password" id="password" required placeholder="Mot de passe">
+
+                <div class="input_pass_eye">
+                    <svg width="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                        <path d="M1.18164 12C2.12215 6.87976 6.60812 3 12.0003 3C17.3924 3 21.8784 6.87976 22.8189 12C21.8784 17.1202 17.3924 21 12.0003 21C6.60812 21 2.12215 17.1202 1.18164 12ZM12.0003 17C14.7617 17 17.0003 14.7614 17.0003 12C17.0003 9.23858 14.7617 7 12.0003 7C9.23884 7 7.00026 9.23858 7.00026 12C7.00026 14.7614 9.23884 17 12.0003 17ZM12.0003 15C10.3434 15 9.00026 13.6569 9.00026 12C9.00026 10.3431 10.3434 9 12.0003 9C13.6571 9 15.0003 10.3431 15.0003 12C15.0003 13.6569 13.6571 15 12.0003 15Z"></path>
+                    </svg>
+                    <input type="password" name="password" id="password" required placeholder="Mot de passe">
+
+                </div>
+
+
                 <div class="error_pass"></div>
                 <div class="errors">
                     <div class="error_length"><svg width="12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
@@ -39,7 +48,11 @@
                 </div>
             </div>
             <div class="input_pass">
-                <input type="password" name="confirm_password" id="confirm_password" required placeholder="Confirmer mot de passe">
+                <div class="input_pass_eye">
+                    <svg width="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                        <path d="M1.18164 12C2.12215 6.87976 6.60812 3 12.0003 3C17.3924 3 21.8784 6.87976 22.8189 12C21.8784 17.1202 17.3924 21 12.0003 21C6.60812 21 2.12215 17.1202 1.18164 12ZM12.0003 17C14.7617 17 17.0003 14.7614 17.0003 12C17.0003 9.23858 14.7617 7 12.0003 7C9.23884 7 7.00026 9.23858 7.00026 12C7.00026 14.7614 9.23884 17 12.0003 17ZM12.0003 15C10.3434 15 9.00026 13.6569 9.00026 12C9.00026 10.3431 10.3434 9 12.0003 9C13.6571 9 15.0003 10.3431 15.0003 12C15.0003 13.6569 13.6571 15 12.0003 15Z"></path>
+                    </svg><input type="password" name="confirm_password" id="confirm_password" required placeholder="Confirmer mot de passe">
+                </div>
                 <div class="error_confirmpass"></div>
             </div>
             <div class="btn">
@@ -60,6 +73,18 @@
         const lowers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
         const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
         const specials = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "|", "}"];
+        const svgs = document.querySelectorAll(".input_pass_eye > svg");
+
+        for (let svg of svgs) {
+            let input = svg.nextElementSibling;
+            svg.addEventListener("mousedown", () => {
+                input.setAttribute("type", "text");
+            })
+            svg.addEventListener("mouseup", () => {
+                input.setAttribute("type", "password");
+            })
+        }
+
 
         email.addEventListener("input", () => {
             const regex = /^[\w\-.]+@([\w-]+.)+[\w-]{2,}$/gm;
@@ -95,19 +120,17 @@
                 if (numbers.includes(character)) rules.numbers++;
             }
 
-            if (rules.uppers > 0) document.querySelector(".error_upper").style.color = "green";
-            if (rules.lowers > 0) document.querySelector(".error_lower").style.color = "green";
-            if (rules.specials > 0) document.querySelector(".error_special").style.color = "green";
-            if (rules.numbers > 0) document.querySelector(".error_number").style.color = "green";
-            if (rules.count >= 8) document.querySelector(".error_length").style.color = "green";
+            document.querySelector(".error_upper").style.color = (rules.uppers > 0) ? "green" : "inherit";
+            document.querySelector(".error_lower").style.color = (rules.lowers > 0) ? "green" : "inherit";
+            document.querySelector(".error_special").style.color = (rules.specials > 0) ? "green" : "inherit";
+            document.querySelector(".error_number").style.color = (rules.numbers > 0) ? "green" : "inherit";
+            document.querySelector(".error_length").style.color = (rules.count >= 8) ? "green" : "inherit";
 
             if (rules.count >= 8 && rules.lowers > 0 && rules.uppers > 0 && rules.specials > 0 && rules.numbers > 0) {
                 password.style.border = "1px solid green";
                 document.querySelector(".error_pass").textContent = "";
                 if (email.value.length > 0 && checkPassword()) {
                     document.getElementById("submit").removeAttribute("disabled");
-                    console.log("test");
-
                 }
             } else {
                 password.style.border = "1px solid red";
@@ -121,7 +144,6 @@
             document.querySelector(".error_confirmpass").textContent = confirmPassword.value !== password.value ? "Les mots de passe ne sont pas identiques" : "";
             if (email.value.length > 0 && checkPassword()) {
                 document.getElementById("submit").removeAttribute("disabled");
-                console.log("test");
             }
         })
 
